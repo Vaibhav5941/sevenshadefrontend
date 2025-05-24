21eea8d725633cde0c3876448364ff89694ef821
demo <gvaibhav5941@gmail.com>
2025-05-24 20:02:28 +0530

    edit content

--- src/userinterface/components/SliderComponent.js
+++ src/userinterface/components/SliderComponent.js
@@ -2,8 +2,9 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 import { serverURL } from "../../services/FetchDjangoApiServices";
-
 export default function SliderComponent(props) {
   var settings = {
     dots: true,
@@ -14,10 +15,14 @@ export default function SliderComponent(props) {
     slidesToScroll: 1,
     arrows: false
   };
+const theme = useTheme();
+const md_matches = useMediaQuery(theme.breakpoints.down('md'));
+const sm_matches = useMediaQuery(theme.breakpoints.down('sm'));  
+
   var data = props.data
   const showSlider = () => {
     return data.map((item) => {
-      return <div><img src={`${serverURL}/static/${item}`} style={{ width: '98%', height: '46%' }} /></div>
+      return <div><img src={`${serverURL}/static/${item}`} style={{ width:sm_matches ? '180%' : md_matches ? '100%' : '98%' , height: '46%' }} /></div>  //sm_matches ? 2 : md_matches ? 4 : 6
     })
 
   }
